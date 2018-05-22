bf37e36a34e734ae7f3562b5292080579f421b2a
mfouda214 <m.fouda@robone.net>
2018-05-22 23:00:59 +0100

    Get london temp

--- NetworkingDemo/ViewController.swift
+++ NetworkingDemo/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBAction func getTempButtonTapped(_ sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        getWeatherData(urlString: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=6c20f50475503fab68506043e715a59e")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getWeatherData(urlString: String) {
+        let url = URL(string: urlString)
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            DispatchQueue.main.async(execute: {
+                self.setLabel(weatherData: data!)
+            })
+        }
+        
+        task.resume()
+        
+    }
+    
+    func setLabel(weatherData: Data) {
+        do {
+            let json = try JSONSerialization.jsonObject(with: weatherData, options: []) as! Dictionary<String, AnyObject>
+            
+            if let main = json["main"] as? Dictionary<String, AnyObject> {
+                if let temp = main["temp"] as? Double {
+                    temperatureLabel.text = String(format: "%.1f", temp)
+                }
+            }
+        } catch {
+            print("Error fetching data")
+        }
+    }
+    
 
 }
 
